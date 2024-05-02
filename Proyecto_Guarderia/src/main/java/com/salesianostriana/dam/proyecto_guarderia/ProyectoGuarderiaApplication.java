4b94897e1d0f3c40a545f65326d00b558c9ec386
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-05-02 13:46:56 +0200

    proyecto de spring creado

---
+++ Proyecto_Guarderia/src/main/java/com/salesianostriana/dam/proyecto_guarderia/ProyectoGuarderiaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyecto_guarderia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGuarderiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGuarderiaApplication.class, args);
+	}
+
+}
---
+++ Proyecto_Guarderia/src/main/java/com/salesianostriana/dam/proyecto_guarderia/model/Alumno.java
@@ -0,0 +1,38 @@
+package com.salesianostriana.dam.proyecto_guarderia.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Data @NoArgsConstructor @AllArgsConstructor 
+public class Alumno {
+
+	@Id @GeneratedValue
+	private long codAlumno;
+	
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private int edad;
+	private int curso;
+	private LocalDate fechaNacimiento;
+	private String direccion;
+	
+	
+	public Alumno(long codAlumno, String nombre, String apellido1, int edad, int curso, 
+				  LocalDate fechaNacimiento, String direccion) {
+		this.codAlumno = codAlumno;
+		this.nombre = nombre;
+		this.apellido1 = apellido1;
+		this.edad = edad;
+		this.curso = curso;
+		this.fechaNacimiento = fechaNacimiento;
+		this.direccion = direccion;
+	}
+	
+}
